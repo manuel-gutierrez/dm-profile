33c8c22aa05fe0b2a02e0f9b0291b546414c062f
David Mejia <davidfdomejiab@gmail.com>
2023-04-29 16:38:15 -0500

    add profile pic to presentation page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import classes from './App.module.css';
 import Navbar from "./Components/Navbar";
 import AsideButton from "./Components/AsideButton";
 import Presentation from "./Components/Presentation";
+import ProfilePicture from "./profile_img.jpeg"
 import { Fragment } from 'react';
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
       </div>
       <div className={classes.main}>
         <aside className={classes.aside}>
+          <div className={classes.profile}>
+            <img className={classes.img} src={ProfilePicture} />
+          </div>
           <ul className={classes.ul}>
             { pages.map((title) => {
               return (
