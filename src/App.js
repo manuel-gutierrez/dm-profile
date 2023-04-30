3d995a735efc954dd1d43332ccc397526dbea941
David Mejia <davidfdomejiab@gmail.com>
2023-04-30 18:43:24 -0500

    add social media links

--- src/App.js
+++ src/App.js
@@ -4,9 +4,23 @@ import AsideButton from "./Components/AsideButton";
 import Body from "./Pages/Body";
 import ProfilePicture from "./profile_img.jpeg"
 import { Fragment } from 'react';
+import Github from './Components/Emoticons/Github';
+import Linkedin from './Components/Emoticons/Linkedin';
+import Instagram from './Components/Emoticons/Instagram';
 
 function App() {
-  const pages = ["Home", "CV", "Cover letter", "Links"]
+  const pages     = ["Home", "CV", "Cover letter", "Links"];
+  const github1   = "https://github.com/DavidMejia025";
+  const github2    = "https://github.com/clayman1325";
+  const linkedin  = "https://www.linkedin.com/in/david-mejia-bonilla-b8005470/";
+  const instagram = "https://www.instagram.com/davidfdomejia/";
+
+  const handleSocialMedia = () => {
+    <a
+    >
+
+    </a>
+  }
   return (
     <Fragment>
       <div className={classes.navbar}>
@@ -16,6 +30,22 @@ function App() {
         <aside className={classes.aside}>
           <div className={classes.profile}>
             <img className={classes.img} src={ProfilePicture} />
+          </div>
+          <div className={classes.emoticonsContainer}>
+            <div className={classes.emoticons}>
+              <div className={classes.emoticon}>
+                <Github url={github1}/>
+              </div>
+              <div className={classes.emoticon}>
+                <Github url={github2}/>
+              </div>
+              <div className={classes.emoticon}>
+                <Linkedin url={linkedin}/>
+              </div>
+              <div className={classes.emoticon}>
+                <Instagram url={instagram}/>
+              </div>
+            </div>
           </div>
           <ul className={classes.ul}>
             { pages.map((title) => {
