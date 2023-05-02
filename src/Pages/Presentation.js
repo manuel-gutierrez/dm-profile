bbaf131552a02f4170bc62ec2fc33ac1525d4754
David Mejia <davidfdomejiab@gmail.com>
2023-05-02 09:38:34 -0500

    fix home page navigation button

--- src/Pages/Presentation.js
+++ src/Pages/Presentation.js
@@ -6,26 +6,28 @@ import CamusEMS from "../Images/ems-monitor.jpeg"
 const Presentation = () => {
     const title = ["Application to the Sofware Engineer position", "Camus Energy"]
     const name = "David Mejia";
-    const subDomain = ["Software Engineer (5 Yrs)", "MS.c Electrical Engineering"]
+    const subDomain = ["Software Engineer", "MS.c Electrical Engineering"]
     return (
         <div className={classes.main}>
             <div className={classes.title}>
                 <p> {title[0]}</p>
             </div>
-            <div className={classes.companyTitle}>
+            {/* <div className={classes.companyTitle}>
                 <img src={CamusEnergyLogo} className={classes.companyImg} />
-            </div>
+            </div> */}
             <div className={classes.background}>
                 <img src={CamusEMS} className={classes.ems} />
             </div>
             <div className={classes.bottom}>
                 <Link className={classes.a} to="Why">
-                    <div className={classes.name}>
-                        <p> {name} </p>
-                    </div>
-                    <div className={classes.subDomain}>
-                        <p> <span>{subDomain[0]} </span></p>
-                        <p> {subDomain[1]} </p>
+                    <div className={classes.title}>
+                        <div className={classes.name}>
+                            <p> {name} </p>
+                        </div>
+                        <div className={classes.subDomain}>
+                            <p> <span>{subDomain[0]} </span></p>
+                            <p> {subDomain[1]} </p>
+                        </div>
                     </div>
                 </Link>
             </div>
