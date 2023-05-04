e84a1588b643d0745da193fdacfaa2b7886bca34
Manuel Gutierrez <m.gutierrez@druo.com>
2023-05-03 22:09:29 -0500

    Revert "refactor:"
    This reverts commit 4cd14eec6fefafa3a743f3fab0fa5608d6ccf0fc.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,25 @@
 import classes from "./Home.module.css";
+import Why from "./why/Why"
+import Divider from "../Components/Divider";
+import Cv from "./cv/Cv";
+import CoverLetter from "./CoverLetter/CoverLetter";
+import Links from "./Links/Links";
+import Presentation from "./Presentation/Presentation";
 
 const Home = () => {
-	return (
-		<div className={classes.main}>
-			<p> Some text</p>
-		</div>
-	);
-};
+    return (
+        <div className={classes.main}>
+            <Presentation />
+            <Divider />
+            <Why />
+            <Divider />
+            <CoverLetter />
+            <Divider />
+            <Cv />
+            <Divider />
+            <Links />
+        </div>
+    )
+}
 
 export default Home;
